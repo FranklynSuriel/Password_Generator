56008264905fdf265024eb844139fdd4c31e5f34
Franklyn <fsuriel@gmail.com>
2022-12-10 19:24:58 -0500

    added if statements

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ var numbersChr = [0,1,2,3,4,5,6,7,8,9]
 var specialChr = ["!","@","#","$","%","^","&","*","(",")","_","-","+","/"];
 var result = [];
 var generateBtn = document.querySelector("#generate");
+var count = 0
+var allChr = []
+
 
 function generatePassword(){
   var passwordLength = window.prompt(" Enter password lenght (Between 8 and 128 Characters): ");
@@ -21,26 +24,58 @@ function generatePassword(){
     window.alert(" You need to choose a password between 8 to 128 characters");
     generatePassword();
   }
-  
+  var lower = [];
+  var upper = [];
+  num = [];
+  specials = [];
+  // Create if statment for each requirement that is true
   //if statement is bad , for is good
-  if (lowerCase === true) {
-    for (i = 0; i <= passwordLength; i = i + 1) {
+  if (lowerCase) {
+    for (i = 0; i < 1; i = i + 1) {
       var index = Math.floor(Math.random() * lowerChr.length);
-      result[i] = lowerChr[index]
-      
-
-      // result[i] =(lowerCase[index]);
-      // result = result.concat(result[i]);
-      
-      // var result = result.push(computerChoice);
-      console.log(index);
-      // console.log(result[i]);
-      // console.log(result);
-
-
+      lower = lower.concat(lowerChr[index]);
+      allChr = allChr.concat(lowerChr);
+      count++;
+     
     }
+    result = result.concat(lower);
   }
 
+  if (upperCase) {
+    for (i = 0; i < 1; i = i + 1) {
+      var index = Math.floor(Math.random() * upperChr.length);
+      upper = upper.concat(upperChr[index]);
+      allChr = allChr.concat(upperChr);
+      count++;
+    }
+    result = result.concat(upper);
+  }
+
+  if (numbersChr) {
+    for (i = 0; i < 1; i = i + 1) {
+      var index = Math.floor(Math.random() * numbersChr.length);
+      num = num.concat(numbersChr[index]);
+      allChr = allChr.concat(numbersChr);
+      count++;
+    }
+    result = result.concat(num);
+  }
+
+  if (specialChr) {
+    for (i = 0; i < 1; i = i + 1) {
+      var index = Math.floor(Math.random() * specialChr.length);
+      specials = specials.concat(specialChr[index]);
+      allChr = allChr.concat(specialChr);
+      count++;
+      
+    }
+    result = result.concat(specials);
+  }
+
+  console.log(count);
+  console.log(result);
+  console.log(allChr);
+  result.sort();
   return(result.join(""));
 
 }
